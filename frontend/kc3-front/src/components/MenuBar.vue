cb7a0147d25a6249f73acff517375c8b347c54b6
ueno-8-ueno <usus.kobaryuji0808@gmail.com>
2023-02-13 23:07:58 +0900

    ハンバーガーメニューを表示&ログイン画面のvueファイルを追加

---
+++ frontend/kc3-front/src/components/MenuBar.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const drawer = ref(false);
+
+const menuItems = [
+    {
+        name: '本棚',
+        url: '#'
+    },
+    {
+        name: 'ISBN登録',
+        url: '#'
+    },
+    {
+        name: '書籍確認',
+        url: '#'
+    },
+    {
+        name: '本棚公開',
+        url: '#'
+    }
+];
+</script>
+
+<template>
+    <div class="menu">
+    <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+    
+    <v-navigation-drawer location="end" v-model="drawer" fixed temporary>
+        <v-list nav dense>
+          <v-list-item-group>
+            <v-list-item v-for="(menuItem, index) in menuItems" :key="index">
+                <v-list-item-title>{{ menuItem.name }}</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
+</template>
+
+<style>
+.menu{
+  padding: 4px;
+}
+</style>
---
+++ frontend/kc3-front/src/views/AppLogin.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import LoginView from "@/components/LoginView.vue";
+</script>
+
+<template>
+    <div class="main-bg">
+      <LoginView></LoginView>
+    </div>
+</template>
