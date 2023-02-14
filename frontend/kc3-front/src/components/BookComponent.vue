a260daad393f730e48c025c4e4ce8298be778002
YumaHozumi <cpucmptrpython72@gmail.com>
2023-02-14 16:06:43 +0900

    本の詳細をモーダルウィンドウで表示する機能を追加

--- frontend/kc3-front/src/components/BookComponent.vue
+++ frontend/kc3-front/src/components/BookComponent.vue
@@ -5,7 +5,6 @@ const dialog = ref(false);
 
 const props = withDefaults(defineProps<Props>(), {
   title: "タイトルの取得に失敗",
-  detail: "詳細の取得に失敗",
   count: 0,
   latest_num: 0,
   image_url: "/src/assets/noimage.png",
@@ -14,7 +13,6 @@ const props = withDefaults(defineProps<Props>(), {
 interface Props {
   isbn: number;
   title: string;
-  detail: string;
   count?: number;
   date: Date;
   latest_num?: number;
@@ -38,12 +36,63 @@ interface Props {
         <v-card-title>
           {{ title }}
         </v-card-title>
+
+        <v-divider class="mx-4 mb-1"></v-divider>
+
         <v-card-text>
-          {{ detail }}
+          <v-row>
+            <v-col cols="12">
+              <v-card color="teal-darken-2" theme="dark" elevation="5">
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <v-avatar size="125" class="me-8">
+                    <v-icon size="100">mdi-book-account</v-icon>
+                  </v-avatar>
+                  <div class="me-auto mt-6 ms-auto">
+                    <v-card-title
+                      >持っている本の数</v-card-title
+                    >
+                    <v-card-text>{{ count }}冊</v-card-text>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+
+            <v-col cols="12">
+              <v-card color="light-green-darken-1" theme="dark">
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <v-avatar size="125" class="me-8">
+                    <v-icon size="100">mdi-calendar-multiselect</v-icon>
+                  </v-avatar>
+                  <div class="me-auto mt-6 ms-auto">
+                    <v-card-title
+                      >登録日</v-card-title
+                    >
+                    <v-card-text>{{ date.toLocaleDateString() }}</v-card-text>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+            <v-col cols="12">
+              <v-card color="green-lighten-1" theme="dark">
+                <div class="d-flex flex-no-wrap justify-space-between">
+                  <v-avatar size="125" class="me-8">
+                    <v-icon size="100">mdi-bookshelf</v-icon>
+                  </v-avatar>
+                  <div class="me-auto mt-6 ms-auto">
+                    <v-card-title
+                      >所持している最新刊</v-card-title
+                    >
+                    <v-card-text>{{ latest_num }}巻</v-card-text>
+                  </div>
+                </div>
+              </v-card>
+            </v-col>
+          </v-row>
         </v-card-text>
 
         <v-card-actions>
-          <v-btn color="primary" block @click="dialog = false">
+          <v-btn color="primary" block @click="dialog = false" max-width="25">
+            <v-icon>mdi-close-box-multiple-outline</v-icon>
             Close
           </v-btn>
         </v-card-actions>
