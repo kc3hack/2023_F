5fa1620e8eb17e532c0677016dbe0cd14fd5c10e
ueno-8-ueno <usus.kobaryuji0808@gmail.com>
2023-02-16 21:20:10 +0900

    html2canvasによるスクショと、Twitterへの投稿(未完成)

--- frontend/kc3-front/src/components/MenuBar.vue
+++ frontend/kc3-front/src/components/MenuBar.vue
@@ -1,56 +1,72 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const drawer = ref(false);
+import { ref } from 'vue';
+import html2canvas from 'html2canvas';
 
+const drawer = ref(false);
 const menuItems = [
-  {
-    name: "本棚",
-    url: "/",
-  },
-  {
-    name: "ISBN登録",
-    url: "/search",
-  },
-  {
-    name: "書籍確認",
-    url: "#",
-  },
-  {
-    name: "本棚公開",
-    url: "#",
-  },
+    {
+        name: '本棚',
+        url: '/'
+    },
+    {
+        name: 'ISBN登録',
+        url: '/search'
+    },
+    {
+        name: '書籍確認',
+        url: '/stock'
+    }
 ];
+
+function openSNS(){
+  drawer.value = false;
+  
+  setTimeout(async () => {
+    console.log("screen shot");
+    let image;
+    //TODO 画面スクショ↓
+    html2canvas(document.querySelector('#book-origin' /* スクショしたい範囲(タグ)の属性 */)).then((canvas) => {
+        //const link = document.createElement('a');//
+        //link.href = canvas.toDataURL();//
+        image = canvas.toDataURL();
+        //link.download = `export_image.png`;//
+        //link.click();//
+      });
+    
+    //TODO Twitterへ飛ぶ↓
+	  let s = "自分だけの本棚を公開しました!";
+	  //let url = document.location.href;
+		//投稿画面を開く
+		let url = "https://twitter.com/intent/tweet?url=" + "&text=" + s +"%20pic.twitter.com/"+image;
+		window.open(url,"_blank","width=1000,height=500");
+
+  }, 1000)
+}
 </script>
 
 <template>
-  <div class="menu">
+    <div class="menu">
     <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
-
+    
     <v-navigation-drawer location="end" v-model="drawer" fixed temporary>
-      <v-list nav dense>
-        <v-list-item-group>
-          <v-list-item>
-            <v-list-item-title
-              v-for="(menuItem, index) in menuItems"
-              :key="index"
-            >
-              <div class="mt-16">
-                <RouterLink
-                  :to="menuItem.url"
-                  style="text-decoration: none; color: inherit"
-                  >{{ menuItem.name }}</RouterLink
-                >
-              </div>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list-item-group>
-      </v-list>
-    </v-navigation-drawer>
-  </div>
+        <v-list nav dense>
+          <v-list-item-group>
+            <v-list-item>
+              <v-list-item-title v-for="(menuItem, index) in menuItems" :key="index">
+                <div class="mt-16">
+                <RouterLink :to="menuItem.url" style="text-decoration: none; color: inherit">{{ menuItem.name }}</RouterLink>
+                </div>
+              </v-list-item-title>
+              <button class="mt-16" @click="openSNS">本棚公開</button>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
+      </v-navigation-drawer>
+    </div>
 </template>
 
 <style>
-.menu {
+.menu{
   padding: 4px;
 }
 </style>
