cb6c7019b18a721e8a40b998d9928f2926f78fe2
YumaHozumi <cpucmptrpython72@gmail.com>
2023-02-18 23:13:20 +0900

    書籍登録後、画面遷移する処理を追加

--- frontend/kc3-front/src/components/SearchISBN.vue
+++ frontend/kc3-front/src/components/SearchISBN.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import axios from "axios";
+import router from "@/router";
 
 let isbn = ref("");
 let image = ref("");
@@ -76,6 +77,8 @@ async function addBook() {
     .then((response) => {
       //BEから データを受け取ったときにやる処理
       console.log(response.data); //テスト
+      router.push({ path: "/" });
+      
     })
     .catch((e) => {
       console.log(e);
